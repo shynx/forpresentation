176d065a3d3bca17299619095af8fc5180957985
SHARON <shynx_swear@yahoo.com>
2013-12-29 13:27:12 -0800

    [veiwdetails1] update veiwdetails1.py #1

---
+++ scripts/viewdetails1.py
@@ -0,0 +1,59 @@
+import psycopg2
+import cgi
+
+def getmovieinfo(movieid):
+    constr = """
+       dbname='sharondb'
+       user='sharon'
+       password='sharon123'
+       host='pythonista.learning.edu'
+    """
+    conn = psycopg2.connect(constr)
+    curr = conn.cursor()
+    curr.execute("select * from movies")
+    rows = curr.fetchall()
+    return rows[0]
+
+
+
+def index(req, movieid):
+    movieid = cgi.escape(movieid)
+    header = """
+	<!DOCTYPE html>
+	<html>
+	<head>
+	<title>Movies Database Example</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<!-- Bootstrap -->
+	<link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+	<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+	<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+	<!--[if lt IE 9]>
+	    <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+	    <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
+	    <![endif]-->
+	</head>
+	    """
+    bodybegin = """
+       <body>
+        <h1>Movie Details</h1>
+    """
+    bodyend = """
+           <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+             <script src="https://code.jquery.com/jquery.js"></script>
+           <!-- Include all compiled plugins (below), or include individual files as needed -->
+           <script src="js/bootstrap.min.js"></script>
+           </body>
+           </html>
+    """
+    movie = getmovieinfo(movieid)
+    tablecontainer = '<div class="container"> '
+    tablecontainer += ' <div class="starter-template"> '
+    tablecontainer +=  ' <h1>Title: '+movie[2]+'</h1> '
+    tablecontainer +=   '<p class="lead">Genre:' + movie[3] + ' <br>'
+    tablecontainer +=  'Release Year'+ movie[4])
+    tablecontainer += '<br /> <a href="http://pythonista.learning.edu/~pythonista/index.py"'
+    tablecontainer += 'class="btn btn-success btn-sm active">Main Page</a></p></div></div>'
+
+    return header + bodybegin  + moviecontainer + bodyend
